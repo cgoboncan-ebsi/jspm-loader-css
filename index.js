8b2e2a6cd1bd6f9534aecf2460432032c3799b91
Carlos Go Boncan <cgoboncan@empyreanbenefits.com>
2016-03-11 21:31:11 -0600

    Fixing syntax error.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {CSSLoader} from './lib/CSSLoader'
 
 const {fetch, bundle} = new CSSLoader([
   Core.values,
-  localByDefault: Core.localByDefault,
+  Core.localByDefault,
   Core.extractImports,
   Core.scope
 ])
